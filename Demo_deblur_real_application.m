f47a52ae9849265843553639ee8767316f754e61
cszn <cskaizhang@gmail.com>
2018-04-26 11:06:35 +0800

    image deblurring with estimated kernel

--- Demo_deblur_real_application.m
+++ Demo_deblur_real_application.m
@@ -2,8 +2,8 @@
 % This is the testing code of IRCNN for image deblurring with estimated
 % kernel by other blind deblurring methods.
 %
-% There are two important parameters to tune: 
-% (1) image noise level of blurred image: Isigma and 
+% There are two important parameters to tune:
+% (1) image noise level of blurred image: Isigma and
 % (2) noise level of the last denoiser: Msigma.
 %
 % @inproceedings{zhang2017learning,
@@ -59,6 +59,7 @@ end
 k  = im2single(k);
 k  = k./(sum(k(:)));
 
+
 %% -------------------important!------------------
 % Parameter settings of IRCNN
 % (1) image noise level of blurred image: Isigma
@@ -69,16 +70,22 @@ Msigma = 5; % ****** from {1 3 5 7 9 11 13 15} ******
 
 
 [a1,b1,~] = size(y);
-% edgetaper to better handle circular boundary conditions
-ks = floor((size(k, 1) - 1)/2);
-y = padarray(y, [1 1]*ks, 'replicate', 'both');
-for a=1:4
-    y = edgetaper(y, k);
+
+%% handle boundary
+boundary_handle = 'case2';
+switch boundary_handle
+    case {'case1'} % option (1), edgetaper to better handle circular boundary conditions, (matlab2015b)
+        % k(k==0) = 1e-10; % uncomment this for matlab 2016--2018?
+        ks = floor((size(k) - 1)/2);
+        y = padarray(y, ks, 'replicate', 'both');
+        for a=1:4
+            y = edgetaper(y, k);
+        end
+    case {'case2'} % option (2)
+        H = size(y,1);    W = size(y,2);
+        y = wrap_boundary_liu(y, opt_fft_size([H W]+size(k)-1));
 end
 
-% another option
-% H = size(y,1);    W = size(y,2);
-% y = wrap_boundary_liu(y, opt_fft_size([H W]+size(k)-1));
 
 [w,h,c]  = size(y);
 V = psf2otf(k,[w,h]);
@@ -129,9 +136,9 @@ for itern = 1:totalIter
     residual = res(end).x;
     z = z - residual;
     
-%     imshow(z)
-%     title(int2str(itern))
-%     drawnow;
+    %     imshow(z)
+    %     title(int2str(itern))
+    %     drawnow;
 end
 
 
@@ -139,16 +146,17 @@ if useGPU
     output = im2uint8(gather(z));
 end
 
+switch boundary_handle
+    case {'case1'} % option (1)
+        output = center_crop(output,a1,b1);
+        y = center_crop(y,a1,b1);
+    case {'case2'} % option (2)
+        output = output(1:a1,1:b1,:);
+        y      = y(1:a1,1:b1,:);
+end
+
 imshow(cat(2,im2uint8(y),output));
-% output = output(1:a1,1:b1,:);
-output = center_crop(output,a1,b1);
 imwrite(output,fullfile(folderResultCur,[Iname,'_ircnn_Isigma_',int2str(Isigma*255),'_Msigma_',int2str(Msigma),'.png']));
-
-
-
-
-
-
 
 
 
